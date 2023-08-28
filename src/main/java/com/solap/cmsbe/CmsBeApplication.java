1c21e208dc132b41a1d8c81088b56da10f719a1b
gihug <45871338+gihug@users.noreply.github.com>
2023-08-28 19:29:31 +0700

    Initial commit

---
+++ src/main/java/com/solap/cmsbe/CmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.solap.cmsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solap/cmsbe/CmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solap.cmsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
